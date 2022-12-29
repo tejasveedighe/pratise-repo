9c944a9ef80d567722626c8fe78d631f5b526cfe
Tiksha <khirsagart@gmail.com>
2022-12-29 12:28:40 +0530

    made a class

--- main.cpp
+++ main.cpp
@@ -1,1 +1,16 @@
 // make a class name Person with name and age as variables
+#include<iostream>
+using namespace std;
+class Person{
+	public:
+	int age;
+	string name;
+	
+};
+int main()
+{
+	Person obj;
+	cin >> obj.age >> obj.name;
+	cout<< obj.age << endl << obj.name;
+	return 0;
+}
